655b11a26b21df93e64e3e9e5b7a8b6612e6830f
Chrines <chrinesskatete@gmail.com>
2026-01-20 14:29:14 +0200

    Implementing Shipping1 class and StandartShipping class

--- eCommerce/Domains/Models/Shipping/Shipping1.cs
+++ eCommerce/Domains/Models/Shipping/Shipping1.cs
@@ -1,8 +1,27 @@
 using System;
+using eCommerce.Domains.Enums;
 
 namespace eCommerce.Domains.Models.Shipping;
 
-public class Shipping1
+public abstract class Shipping1
 {
+    public int shippingId {get;set;}
+    public string trackingNumber{get; set;}
+    public ShippingStatus status {get;set;}
+    public DateTime estimatedDelivery {get;set;}
 
+    // Abstract methods
+        public abstract decimal CalculateCost(double weight);
+        public abstract int GetDeliveryDays();
+
+        protected Shipping1()
+        {
+            status = ShippingStatus.Pending;
+            trackingNumber = GenerateTrackingNumber();
+        }
+
+        private string GenerateTrackingNumber()
+        {
+            return "TRK" + DateTime.Now.Ticks.ToString().Substring(0, 10);
+        }
 }
--- eCommerce/Domains/Models/Shipping/StandartShipping.cs
+++ eCommerce/Domains/Models/Shipping/StandartShipping.cs
@@ -2,7 +2,16 @@ using System;
 
 namespace eCommerce.Domains.Models.Shipping;
 
-public class StandartShipping
+public class StandartShipping: Shipping1
 {
+    public override decimal CalculateCost(double weight)
+        {
+            // $5 base + $1 per kg
+            return 5m + (decimal)weight * 1m;
+        }
 
+        public override int GetDeliveryDays()
+        {
+            return 7; // WITHIN 7 days
+        }
 }
